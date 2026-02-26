789dfc90de3c2293fc116dd0adebb448c0dd92c1
Hiteshi Meisheri <152746571+wannabecodeboss@users.noreply.github.com>
2026-02-27 00:58:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,80 @@
+// Firebase config
+const firebaseConfig = {
+
+apiKey: "AIzaSyDXBV-8rXET5-OSKr6fG9FW3m6IVw1Ujsk",
+
+databaseURL:
+"https://battery-gas-detection-default-rtdb.asia-southeast1.firebasedatabase.app"
+
+};
+
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
+
+const readingsRef = db.ref("readings");
+
+
+// Chart setup
+
+const ctx = document.getElementById("myChart");
+
+let chart = new Chart(ctx, {
+
+type: "line",
+
+data: {
+
+labels: [],
+
+datasets: [
+
+{
+
+label: "Sensor Value",
+
+data: [],
+
+}
+
+]
+
+},
+
+options: {
+
+animation:false
+
+}
+
+});
+
+
+
+// Live updates
+
+readingsRef.on("value", (snapshot)=>{
+
+let data = snapshot.val();
+
+let labels = [];
+
+let values = [];
+
+for(let key in data){
+
+labels.push(key);
+
+values.push(data[key]);
+
+}
+
+chart.data.labels = labels;
+
+chart.data.datasets[0].data = values;
+
+chart.update();
+
+});
